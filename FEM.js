0520bf31bb682f8572e2b6af96e67d7236ae5632
Clara Jordan <clara@jordan-maynard.org>
2014-07-25 00:06:33 +0100

    Made things prettier and added more load cases

--- FEM.js
+++ FEM.js
@@ -20,11 +20,12 @@ function addLoad() {
 	var W = parseFloat(document.getElementById('W').value);
 	var a = parseFloat(document.getElementById('a').value);
 	var l = parseFloat(document.getElementById('l').value);
+	var b=1-a;
 	var x =a*l;
+	var xb =b*l;
 	var x2 =l-2*a*l;
 	var Wper = W/x;
 	var Wper2 =W/x2;
-
 
 	if (a>1 || isNaN(W) || isNaN(a) || isNaN(l)){
 		alert("Please check you have entered all values correctly");
@@ -38,33 +39,42 @@ function addLoad() {
 			document.getElementById('l').value=l+"m";
 			document.getElementById('l').disabled=true
 		}
-
 		//calculate the FEM and update loading log
 		if (document.getElementById('pload').checked){
-	      	MFAB = MFAB - a*(1-a)*(1-a)*W*l;
-	     	MFBA = MFBA + a*a*(1-a)*W*l;
+	      	MFAB = MFAB - a*b*b*W*l;
+	     	MFBA = MFBA + a*a*b*W*l;
 	     	document.getElementById('loading').innerHTML+= "Point load of "+W+"kN, "+x+"m from the left. <br>"
 	    }else if (document.getElementById('udl_1').checked){
-	      	MFAB = MFAB - (a*(6-8*a+3*a*a))/12;
-	     	MFBA = MFBA + (a*a*(4-3*a))/12;
+	      	MFAB = MFAB - W*l*(a*(6-8*a+3*a*a))/12;
+	     	MFBA = MFBA + W*l*(a*a*(4-3*a))/12;
 	     	document.getElementById('loading').innerHTML+= "UDL of "+W+"kN ("+Wper+"kN/m), spanning "+x+"m from the left side. <br>"
+	    }else if (document.getElementById('udl_1r').checked){
+	      	MFAB = MFAB - W*l*(b*b*(4-3*b))/12;
+	     	MFBA = MFBA + W*l*(b*(6-8*b+3*b*b))/12;
+	     	document.getElementById('loading').innerHTML+= "UDL of "+W+"kN ("+Wper+"kN/m), spanning "+xb+"m from the right side. <br>"
 	    }else if (document.getElementById('udl_2').checked){
 	    	if (a<0.5){
-	    		MFAB = MFAB - (1+2*a-2*a*a)/12;
-	     		MFBA = MFBA + (1+2*a-2*a*a)/12;
+	    		MFAB = MFAB - W*l*(1+2*a-2*a*a)/12;
+	     		MFBA = MFBA + W*l*(1+2*a-2*a*a)/12;
 	     		document.getElementById('loading').innerHTML+= "UDL of "+W+"kN ("+Wper2+"kN/m), starting "+x+"m from either side and spanning "+x2+"m. <br>"
 	    	}else{
 	    		alert("Alpha must be smaller than 0.5 for this loading system")
 	    		count--;
 	    	}
-    	}
-    }
+		}else if (document.getElementById('mom').checked){
+	      	MFAB = MFAB - W*l*b*(1-3*a);
+	     	MFBA = MFBA + W*l*a*(2-3*a);
+	     	document.getElementById('loading').innerHTML+= "...";
+			document.getElementById('MFBA').innerHTML = "...";
+		}
+		calc();
+	}
 }
 
 //reset
 function reset(){
 	count=0;
-	document.getElementById('currentLoads').innerHTML = "No loads currently added."
+	document.getElementById('currentLoads').innerHTML = ""
 	MFAB=0;
 	MFBA=0;
 	document.getElementById('MFAB').innerHTML = "";
